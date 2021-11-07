4a42b14d2b2712fd5bca677ce5be4a2ce146d921
Evgeniy Nechaev <kit8@bk.ru>
2021-11-07 18:55:29 +0300

    init commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,35 @@
+import kotlin.math.roundToInt
+
+fun main () {
+
+    var sec =90000
+    var minutes = (sec / 60.0).roundToInt()
+    var hours = (minutes / 60.0).roundToInt()
+    var wordMinutes = minutes.toString()
+    var wordHours = hours.toString()
+
+
+    fun agoToText() {
+        val printMinutes = when (wordMinutes) {
+            "1", "21", "31", "41", "51" -> "минуту"
+            "2", "3", "4", "22", "23", "24", "32", "33", "34", "42", "43", "44", "52", "53", "54" -> "минуты"
+            else -> "минут"
+        }
+        val printHours = when (wordHours) {
+            "1", "21" -> "час"
+            "2", "3", "4", "22", "23", "24" -> "часа"
+            else -> "часов"
+        }
+        when (sec) {
+            in 0..60 -> println("Пользователь был только что")
+            in 61..3600 -> println("Пользователь был $minutes $printMinutes назад")
+            in 3601..86400 -> println("Пользователь был $hours $printHours  назад")
+            in 86401..172800 -> println("Пользователь был сегодня")
+            in 172801..259200 -> println("Пользователь был вчера")
+            else -> println("Пользователь был давно")
+        }
+    }
+
+    agoToText()
+}
+
